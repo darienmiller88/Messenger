92a87b75ae5fed5b918b0b57dcdb9c05a26857f4
darienmiller88 <darienmiller88@yahoo.com>
2022-08-15 16:23:23 -0400

    adding chat window

---
+++ client/src/components/ChatWindowHeader/ChatWindowHeader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatWindowHeader() {
+    return (
+        <div>ChatWindowHeader</div>
+    )
+}
--- client/src/containers/ChatWindow/ChatWindow.js
+++ client/src/containers/ChatWindow/ChatWindow.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import styles from "./ChatWindow.module.scss"
+import { FaArrowLeft } from "react-icons/fa"
+import { BsFillTelephoneFill, BsCameraVideoFill, BsInfoCircleFill } from "react-icons/bs"
+import me from "../../img/me.PNG"
 
 export default function ChatWindow({ isChatWindowActive, setIsChatWindowActive }) {
     return (
         isChatWindowActive
         ?
         <div className={styles.chat_window}>
-            ChatWindow
-            <br />
-            <button onClick={setIsChatWindowActive} className={styles.show_chats}>Click to show chats</button>
+            <div className={styles.chat_header}>
+                <div className={styles.arrow_wrapper}>
+                    <FaArrowLeft onClick={setIsChatWindowActive}  />
+                </div>
+                <div className={styles.img_wrapper}>
+                    <img src={me} alt="profile-pic"/>
+                    <div className={styles.username}>darien88</div>
+                </div>
+                <div className={styles.links_wrapper}>
+                    <BsFillTelephoneFill className={styles.icon} />
+                    <BsCameraVideoFill className={styles.icon}  />
+                    <BsInfoCircleFill className={styles.icon} />
+                </div>
+            </div>
+
+            <div className={styles.messages_wrapper}>
+                ChatWindow
+
+            </div>
+
+            <div className={styles.input_wrapper}>
+                <input placeholder='Aa'/>
+            </div>
         </div>
         :
         null
